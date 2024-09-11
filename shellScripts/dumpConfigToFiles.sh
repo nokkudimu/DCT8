4d7057664d0db0caa127b4312249021a3246e2c1
nokkudimu <nokkudimu@gmail.com>
2024-09-12 00:11:07 +0300

    dumpConfigToFiles shell script added

---
+++ shellScripts/dumpConfigToFiles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/1cv8t/x86_64/8.3.25.1257/1cv8t DESIGNER /F /home/nokkudimu/1C/tekken8_data_collector_infobase /DumpConfigToFiles /home/nokkudimu/repos/tekken8_data_collector/1C_InfoBase
