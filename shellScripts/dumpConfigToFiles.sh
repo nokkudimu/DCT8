d7f2d796dc3c88192cb98f3b46bca962fca005a8
nokkudimu <nokkudimu@gmail.com>
2024-09-19 00:02:55 +0300

    dumpConfig edit

--- shellScripts/dumpConfigToFiles.sh
+++ shellScripts/dumpConfigToFiles.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/1cv8t/x86_64/8.3.25.1257/1cv8t DESIGNER /F /home/nokkudimu/1C/tekken8_data_collector_infobase /DumpConfigToFiles /home/nokkudimu/repos/tekken8_data_collector/1C_InfoBase
+/opt/1cv8/x86_64/8.3.25.1336/1cv8 DESIGNER /UseHwLicenses- /S"1C-Servers\tekken8db" /DumpConfigToFiles /home/nokkudimu/repos/tekken8_data_collector/1C_InfoBase
