6e6a126ac7b9523e9cf8ede68b4d62b335aa5cea
nokkudimu <nokkudimu@gmail.com>
2024-09-22 00:14:57 +0300

    game hack 1 complete (not working)

---
+++ vzlom/main.cpp
@@ -0,0 +1,12 @@
+#include "ProcessManager.h"
+
+int main() {
+    const char Signature[] = "\x48\x8b\x45\x28\x83\x28\x01\x48";
+    char OpCode[] = "\x90\x90\x90";
+
+    ProcessManager procManager("ac_client");
+
+    procManager.SignaturePayload(Signature, OpCode, strlen(Signature), strlen(OpCode), 64, 4);
+
+    exit(EXIT_SUCCESS);
+}
